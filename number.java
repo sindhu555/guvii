5a8f689fc189532cab3ea32fd88f630de869b91a
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 10:04:51 +0530

    Update and rename README.md to number.java

---
+++ number.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		long n=sc.nextLong();
+		if(n>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(n<0)
+		{
+			System.out.println("Negitive");
+		}
+		else
+		{
+			System.out.println("number is 0");
+		}
+	}
+}
